35925813bef23d86289c7f20d9d6db3665adf216
Vanshika Gupta <vanshikagupta0009@gmail.com>
2024-09-29 12:43:09 +0530

    commit

--- app/src/main/java/com/vanshika/libraryapp/AdminActivity.kt
+++ app/src/main/java/com/vanshika/libraryapp/AdminActivity.kt
@@ -2,15 +2,25 @@ package com.vanshika.libraryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.vanshika.libraryapp.databinding.ActivityAdminBinding
 
 class AdminActivity : AppCompatActivity() {
     var binding : ActivityAdminBinding ?= null
+    var navController : NavController ?= null
+    var appBarConfiguration : AppBarConfiguration ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        binding?.fabAdd?.setOnClickListener {}
+        navController = findNavController(R.id.adminHost)
+        binding?.bottomNavigation?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navHome -> navController?.navigate(R.id.adminHomeFragment)
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
--- app/src/main/java/com/vanshika/libraryapp/home/AdminHomeFragment.kt
+++ app/src/main/java/com/vanshika/libraryapp/home/AdminHomeFragment.kt
@@ -1,11 +1,10 @@
-package com.vanshika.libraryapp.books
+package com.vanshika.libraryapp.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.vanshika.libraryapp.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -15,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BooksFragment.newInstance] factory method to
+ * Use the [AdminHomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BooksFragment : Fragment() {
+class AdminHomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +35,7 @@ class BooksFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books, container, false)
+        return inflater.inflate(R.layout.fragment_admin_home, container, false)
     }
 
     companion object {
@@ -46,12 +45,12 @@ class BooksFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BooksFragment.
+         * @return A new instance of fragment AdminHomeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BooksFragment().apply {
+            AdminHomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
