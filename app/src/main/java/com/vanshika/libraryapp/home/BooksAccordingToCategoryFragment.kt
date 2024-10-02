f5de1d6e37b7b12b1b5b31e06c6c2e147a9039b3
Vanshika Gupta <vanshikagupta0009@gmail.com>
2024-10-02 13:15:41 +0530

    commit

--- app/src/main/java/com/vanshika/libraryapp/home/BooksAccordingToCategoryFragment.kt
+++ app/src/main/java/com/vanshika/libraryapp/home/BooksAccordingToCategoryFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.vanshika.libraryapp.R
-import com.vanshika.libraryapp.databinding.FragmentAdminHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,14 +14,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AdminHomeFragment.newInstance] factory method to
+ * Use the [BooksAccordingToCategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AdminHomeFragment : Fragment() {
+class BooksAccordingToCategoryFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    var binding : FragmentAdminHomeBinding ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -37,15 +35,7 @@ class AdminHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentAdminHomeBinding.inflate(inflater)
-        return binding?.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding?.fabAdd?.setOnClickListener {
-            findNavController().navigate(R.id.booksAccordingToCategoryFragment)
-        }
+        return inflater.inflate(R.layout.fragment_books_according_to_category, container, false)
     }
 
     companion object {
@@ -55,12 +45,12 @@ class AdminHomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminHomeFragment.
+         * @return A new instance of fragment BooksAccordingToCategoryFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AdminHomeFragment().apply {
+            BooksAccordingToCategoryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
