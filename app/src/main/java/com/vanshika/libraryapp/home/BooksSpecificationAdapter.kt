d2dfdc049cbb07e92e78ee1757506925e3873904
tanve13 <tanusaini.jandir@gmail.com>
2024-10-03 13:23:50 +0530

    updated_specification_fragment

---
+++ app/src/main/java/com/vanshika/libraryapp/home/BooksSpecificationAdapter.kt
@@ -0,0 +1,24 @@
+package com.vanshika.libraryapp.home
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksSpecificationAdapter(var SpecifiedList:ArrayList<BooksSpecificationDataClass>):
+    RecyclerView.Adapter<BooksSpecificationAdapter.ViewHolder>() {
+    class ViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    var
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
