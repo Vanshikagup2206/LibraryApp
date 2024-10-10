174cef316f9a2a4bad9571b2ec37d94fd669c8e6
tanve13 <tanusaini.jandir@gmail.com>
2024-10-10 10:18:19 +0530

    commit

--- app/src/main/java/com/vanshika/libraryapp/BooksAdditionFragment.kt
+++ app/src/main/java/com/vanshika/libraryapp/BooksAdditionFragment.kt
@@ -133,7 +133,7 @@ class BooksAdditionFragment : Fragment() {
 
         }
     }
-    
+
 
     companion object {
         /**
--- app/src/main/java/com/vanshika/libraryapp/home/BooksSpecificationDataClass.kt
+++ app/src/main/java/com/vanshika/libraryapp/home/BooksSpecificationDataClass.kt
@@ -4,14 +4,15 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(foreignKeys = [
-    ForeignKey(
-        entity = BooksDataClass :: class,
-        parentColumns = ["booksId"],
-        childColumns = ["booksSpecificationId"],
-        onDelete = ForeignKey.CASCADE
-    )
-])
+//@Entity(foreignKeys = [
+//    ForeignKey(
+//        entity = BooksDataClass :: class,
+//        parentColumns = ["booksId"],
+//        childColumns = ["booksSpecificationId"],
+//        onDelete = ForeignKey.CASCADE
+//    )
+//])
+@Entity
 data class BooksSpecificationDataClass(
     @PrimaryKey(autoGenerate = true)
     var booksSpecificationId : Int = 0,
@@ -19,6 +20,6 @@ data class BooksSpecificationDataClass(
     var booksName : String ?= "",
     var booksAuthorName : String ?= "",
     var booksDescription : String ?= "",
-    var booksStatus : Int ?= 0
+    var booksStatus : String ?= ""
     //var isWishlist : Boolean ?= false,
 )
